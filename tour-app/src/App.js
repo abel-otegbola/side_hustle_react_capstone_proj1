fc626f75bd93c934c2b2392e2bc6f133246769eb
Test Projects 83 <testprojects83@gmail.com>
2021-11-30 23:03:39 +0000

    nothing much
    Co-Authored-By: Abel <59369762+abel-otegbola@users.noreply.github.com>
    Co-Authored-By: Thompson Esther Chizoba <76252796+Apreal-T@users.noreply.github.com>

--- tour-app/src/App.js
+++ tour-app/src/App.js
@@ -5,7 +5,7 @@ import Tours from './Tours';
 function App() {
   return (
     <div className='App'>
-      {/* We need to implement a conditional rendering 'Loading....' based on if the app is loading the data from the API */}
+      {/* We need to implement a conditional rendering 'Loading....' state based on if the app is loading the data from the API */}
       <Tours />
     </div>
   );
